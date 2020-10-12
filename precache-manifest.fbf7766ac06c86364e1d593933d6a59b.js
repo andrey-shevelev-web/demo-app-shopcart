6fa742642ca8fd727988ba7528c72d01892c839d
andrey-shevelev-web <andrey.shevelev.web@gmail.com>
2020-10-12 10:19:25 +0700

    Updates

--- precache-manifest.fbf7766ac06c86364e1d593933d6a59b.js
+++ precache-manifest.fbf7766ac06c86364e1d593933d6a59b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1d9f9c48c838c7838f2995cfc84c5dcc",
+    "revision": "fa5fec538d572b54e854208d505578d9",
     "url": "/demo-app-shopcart/index.html"
   },
   {
-    "revision": "919a778c262e46b318c5",
+    "revision": "605b28961035a7dff143",
     "url": "/demo-app-shopcart/static/css/main.b2efc14e.chunk.css"
   },
   {
-    "revision": "9488a95167ac1e8513da",
-    "url": "/demo-app-shopcart/static/js/2.42690498.chunk.js"
+    "revision": "48d549e8e7a1df4a9434",
+    "url": "/demo-app-shopcart/static/js/2.44df1b60.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/demo-app-shopcart/static/js/2.42690498.chunk.js.LICENSE.txt"
+    "url": "/demo-app-shopcart/static/js/2.44df1b60.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "919a778c262e46b318c5",
-    "url": "/demo-app-shopcart/static/js/main.402319f1.chunk.js"
+    "revision": "605b28961035a7dff143",
+    "url": "/demo-app-shopcart/static/js/main.2671b5bb.chunk.js"
   },
   {
     "revision": "ab62dad30634ec185302",
