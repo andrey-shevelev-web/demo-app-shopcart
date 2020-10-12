07dccc3f280fb1d3a65f60042cc54a53a8d679bf
andrey-shevelev-web <andrey.shevelev.web@gmail.com>
2020-10-12 10:07:46 +0700

    Updates

---
+++ precache-manifest.fbf7766ac06c86364e1d593933d6a59b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5fec538d572b54e854208d505578d9",
+    "url": "/demo-app-shopcart/index.html"
+  },
+  {
+    "revision": "605b28961035a7dff143",
+    "url": "/demo-app-shopcart/static/css/main.b2efc14e.chunk.css"
+  },
+  {
+    "revision": "48d549e8e7a1df4a9434",
+    "url": "/demo-app-shopcart/static/js/2.44df1b60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-app-shopcart/static/js/2.44df1b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "605b28961035a7dff143",
+    "url": "/demo-app-shopcart/static/js/main.2671b5bb.chunk.js"
+  },
+  {
+    "revision": "ab62dad30634ec185302",
+    "url": "/demo-app-shopcart/static/js/runtime-main.ea59231b.js"
+  },
+  {
+    "revision": "beb951d6cc8c110db6508d22ead03ab9",
+    "url": "/demo-app-shopcart/static/media/err_icon.beb951d6.png"
+  }
+]);
